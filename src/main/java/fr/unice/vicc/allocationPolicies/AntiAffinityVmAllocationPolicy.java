4d5428a3c0f9afb1cf60fdeb492f688d50f406e8
MarcosBernal <marcosbernal3@hotmail.com>
2017-02-17 17:47:33 +0100

    Added javadoc to AntiAffinity policy

--- src/main/java/fr/unice/vicc/allocationPolicies/AntiAffinityVmAllocationPolicy.java
+++ src/main/java/fr/unice/vicc/allocationPolicies/AntiAffinityVmAllocationPolicy.java
@@ -9,24 +9,47 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * Created by fhermeni2 on 16/11/2015.
+ * Author: Marcos
+ * Date: 17/02/2017.
+ <p/>
+ * This class defines a policy where we try to respect the SLA as much as possible while also
+ * trying to store the VMs in different nodes acording to their IDs. All Vms with an id between [X00-X99]
+ * must be on distinct nodes, where X is the same digit (0,1,2...).
+ *
+ * The main problem here is that when a fault tolerant schedule places two VM replicas over the same host,
+ * if the host fails both VMs get lost. With this policy, the energy is slightly increased but the penalties
+ * are reduced in half resulting into increasing the Revenue.
+ *
+ * @see AbstractAllocationPolicy
  */
 public class AntiAffinityVmAllocationPolicy extends AbstractAllocationPolicy {
 
-
+  /**
+   * The default constructor from AbstractAllocationPolicy is enough.
+   *
+   * @param list a list of hosts
+   */
   public AntiAffinityVmAllocationPolicy(List<? extends Host> list) {
     super(list);
   }
 
-  @Override
-  public List<Map<String, Object>> optimizeAllocation(List<? extends Vm> list) {
-    //In the naive policy it returns null
-    return null;
-  }
-
+  /**
+   * A map with the ids of the hosts and the affinities of the VMs stored in each host.
+   * At the start, all host has no VM assigned, so no affinities are stored.
+   *
+   * @key a Host id
+   * @value a list of affinities of the VM stored in each host
+   */
   public Map<Integer, List<Integer>> affinityMap = new HashMap<Integer, List<Integer>>();
 
-
+  /**
+   * It will allocate the VM in a host which VMs does not share the same Affinity.
+   * In order to do that, a the HashMap affinityMap register the affinity of each host in order to
+   * avoid having two VMs with the same affinity.
+   *
+   * @param vm the vm to be allocated
+   * @return true if the allocation was successful or false otherwise
+   */
   @Override
   public boolean allocateHostForVm(Vm vm) {
 
@@ -52,7 +75,13 @@ public class AntiAffinityVmAllocationPolicy extends AbstractAllocationPolicy {
     return false;
   }
 
-  //Given a Vm get its host and deallocate the vm
+  /**
+   * De-allocates the vm passed by parameter and remove its value from the host that was
+   * storing it.
+   *
+   * @param vm the virtual machine to be de-allocated.
+   * @throws NullPointerException at runtime if the vm was not previously allocated.
+   */
   @Override
   public void deallocateHostForVm(Vm vm) {
         if(hoster.containsKey(vm.getUid())) {
@@ -65,8 +94,8 @@ public class AntiAffinityVmAllocationPolicy extends AbstractAllocationPolicy {
             affinityMap.get(h.getId()).remove(new Integer(vmAffinity));
             if (affinityMap.get(h.getId()).isEmpty())
               affinityMap.remove(h.getId());
-            h.vmDestroy(vm);
-            hoster.remove(vm.getUid());
+
+            super.deallocateHostForVm(vm);
           }
         }
     }
