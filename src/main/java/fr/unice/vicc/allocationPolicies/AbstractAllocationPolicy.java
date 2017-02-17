b6c5f131edd2374cb42534bbf03d16dabdd09e03
MarcosBernal <marcosbernal3@hotmail.com>
2017-02-17 16:48:37 +0100

    Fixed deallocate abstract class method

--- src/main/java/fr/unice/vicc/allocationPolicies/AbstractAllocationPolicy.java
+++ src/main/java/fr/unice/vicc/allocationPolicies/AbstractAllocationPolicy.java
@@ -120,7 +120,7 @@ public abstract class AbstractAllocationPolicy extends VmAllocationPolicy {
    */
   @Override
   public void deallocateHostForVm(Vm vm) {
-    hoster.get(vm.getUid()).vmDestroy(vm);
+    hoster.remove(vm.getUid()).vmDestroy(vm);
   }
 
 }
